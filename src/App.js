2007cc83611be2f242deb7df4f45d1daff430030
Andrey <andrydmit@gmail.com>
2020-01-21 16:36:12 +0200

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import MainPanel from 'sections/MainPanel';
+import InfoPanel from 'sections/InfoPanel';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import 'typeface-roboto';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+
+      <Header />
+      <Container>
+        <MainPanel />
+        <InfoPanel />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
