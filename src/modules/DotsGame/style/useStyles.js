18cc65904036102a441e599a63f22ac86478f3af
Andrey <andrydmit@gmail.com>
2020-02-10 09:38:12 +0200

    bug with height of root container fixed

--- src/modules/DotsGame/style/useStyles.js
+++ src/modules/DotsGame/style/useStyles.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100%',
-    marginTop: 20
+    paddingTop: 20,
+    paddingBottom: 20
   }
 }));
 
